188b15ce3cccc28b2cf363f552af188d8c4d71ea
virtualsites <42749479+virtualsites@users.noreply.github.com>
2020-04-22 19:55:21 +0200

    Update auth.php

--- producers/config/auth.php
+++ producers/config/auth.php
@@ -5,7 +5,7 @@ namespace Isystems\Config;
 class Auth {
     public static function getCredentials() {
         $config = array(
-            'url' => '',// 'http://grzegorz.demos.i-sklep.pl/rest_api/shop_api/v1',
+            'url' => '',
             'authorizationData' => 'rest:vKTUeyrt'
         );
         return $config;
