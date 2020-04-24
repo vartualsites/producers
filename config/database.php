66b3df4dda3f7988c85aed909873201f80dfd737
Bożena Pietrzak <bozena.pietrzak@tsh.io>
2020-04-24 09:45:48 +0200

    Change db to singleton

--- config/database.php
+++ config/database.php
@@ -2,15 +2,35 @@
 
 namespace Isystems\Config;
 
-class Database {
-    public static function getCredentials() {
-        $config = array(
+class Database
+{
+    private static $instance;
+    private $config;
+    
+    private function __construct()
+    {
+        $this->config = array(
             'host' => 'localhost',
             'user' => 'mnhck91_producer',
             'password' => '2ky0fBfB',
             'db' => 'mnhck91_producer',
         );
+    }
+    
+    public function __clone()
+    {
+    }
+    
+    public static function getCredentials() {
+        if (null === self::$instance) {
+            self::$instance = new Database();
+        }
 
-        return $config;
+        return self::$instance->getConfiguration();
+    }
+    
+    private function getConfiguration()
+    {
+        return $this->config;
     }
 }
